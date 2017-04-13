d1d966e52846ac76c2c99905c80e242e7e01df87
kedarla <kedarrampathak@gmail.com>
2017-04-13 15:57:12 +0530

    puma config file

--- config/puma.rb
+++ config/puma.rb
@@ -9,7 +9,7 @@
 threads 1, 6
 # Specifies the `port` that Puma will listen on to receive requests, default is 3000.
 #
-port        ENV.fetch("PORT") { 3000 }
+port        5000#ENV.fetch("PORT") { 5000 }
 
 # Specifies the `environment` that Puma will run in.
 #
